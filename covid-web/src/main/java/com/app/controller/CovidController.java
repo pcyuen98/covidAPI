5071b5383d22df79e3c8a18f1e045108a5ddd121
unknown <pcyuen98@gmail.com>
2021-03-08 19:32:09 +0800

    updates

--- covid-web/src/main/java/com/app/controller/CovidController.java
+++ covid-web/src/main/java/com/app/controller/CovidController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.app.model.CovidCasesArea;
@@ -16,6 +17,11 @@ import lombok.extern.slf4j.Slf4j;
 public class CovidController {
 
 	private final static String GET_COVID = "/covid/get";
+
+	private final static String GET_HELLO_API = "/covid/hello";
+
+	private final static String GET_LOG_API = "/covid/logging";
+
 	@Autowired
 	private CovidService covidService;
 
@@ -34,4 +40,31 @@ public class CovidController {
 		return covidCasesAreas;
 	}
 
+
+
+	// TODO: Practical 1 - Complete the API below
+	// It should return hello when you hit http://localhost:8081/covid/hello on
+	// browser
+
+	@GetMapping(GET_HELLO_API)
+	String getHello() throws Exception {
+		log.info("getHello() started");
+
+		return "Hello API";
+	}
+
+	// TODO: Practical 2 - Capture the error message below from log file
+	// It should return some error when you pass a string as parameter to the HTTP
+	// get
+	// Example, http://localhost:8081/covid/hello?aNumberOnly=string
+
+	@GetMapping(GET_LOG_API)
+	String getLogging(@RequestParam String aNumberOnly) throws Exception {
+		log.info("getLogging() started, requestParamvalue={}", aNumberOnly);
+
+		if (aNumberOnly != null) {
+			Integer.parseInt(aNumberOnly);
+		}
+		return "you have input =>" + aNumberOnly;
+	}
 }
--- covid-web/src/main/java/com/app/controller/MyCovidController.java
+++ covid-web/src/main/java/com/app/controller/MyCovidController.java
@@ -18,10 +18,6 @@ public class MyCovidController {
 
 	private final static String GET_MY_LAST_5_COVID = "/covid/get5/my";
 
-	private final static String GET_HELLO_API = "/covid/hello";
-
-	private final static String GET_LOG_API = "/covid/logging";
-
 	private final static String MINING_MY_COVID = "/covid/mining/my";
 
 	@Autowired
@@ -51,30 +47,4 @@ public class MyCovidController {
 				"getLast5Records() ends. It supposes to return last 5 records from listLast5Records(). (CovidCasesRepository)");
 		return covidMiningAPITotalCases.getLast5RecordsMY();
 	}
-
-	// TODO: Practical 1 - Complete the API below
-	// It should return hello when you hit http://localhost:8081/covid/hello on
-	// browser
-
-	@GetMapping(GET_HELLO_API)
-	String getHello() throws Exception {
-		log.info("getHello() started");
-
-		return "Hello API";
-	}
-
-	// TODO: Practical 2 - Capture the error message below from log file
-	// It should return some error when you pass a string as parameter to the HTTP get
-	// Example, http://localhost:8081/covid/hello?aNumberOnly=string  
-
-	@GetMapping(GET_LOG_API)
-	String getLogging(@RequestParam String aNumberOnly) throws Exception {
-		log.info("getLogging() started, requestParamvalue={}", aNumberOnly);
-
-		if (aNumberOnly != null) {
-			Integer.parseInt(aNumberOnly);
-		}
-		return "you have input =>" + aNumberOnly;
-	}
-
 }
