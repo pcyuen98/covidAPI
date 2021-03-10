bfa8075695ac7d3718d0888b8a94586bfd9d1db0
unknown <pcyuen98@gmail.com>
2021-03-10 19:08:26 +0800

    updates

---
+++ covid-web/src/main/java/test/LombokTest.java
@@ -0,0 +1,12 @@
+package test;
+
+import com.app.model.CovidCasesDesc;
+
+public class LombokTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new CovidCasesDesc();
+	}
+
+}
