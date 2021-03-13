02442dab231bfbc2bb961d44bc614c611aa7224e
unknown <pcyuen98@gmail.com>
2021-03-13 15:08:49 +0800

    updates

--- covid-mining/src/main/java/com/app/mining/controller/MyCovidMiningController.java
+++ covid-mining/src/main/java/com/app/mining/controller/MyCovidMiningController.java
@@ -1,36 +1,22 @@
 package com.app.mining.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.app.mining.service.covid.api.CovidMiningAPITotalCases;
 
 import lombok.extern.slf4j.Slf4j;
 
-@RestController
 @Slf4j
 public class MyCovidMiningController {
 
 	private final static String MINING_MY_COVID = "/covid/mining/my";
 
-	@Autowired
-	CovidMiningAPITotalCases covidMiningAPITotalCases;
-
+	// TODO: Practical 5, move the required logic from covid-web project to here
+	
+	// CovidMiningApiTotalCasesImpl need to be fixed too. Refer to the file TODO remarks
 	@GetMapping(MINING_MY_COVID)
 	String mining() {
 		log.info("mining() started");
-		String strReturn = null;
-		
-		try {
-			strReturn = covidMiningAPITotalCases.doMining();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			log.error("mining() exception " + e.getMessage());
-		}
-
-		log.info(MINING_MY_COVID + " return = {}" + strReturn);
-		return strReturn;
+	
+		return null;
 	}
 	
 }
