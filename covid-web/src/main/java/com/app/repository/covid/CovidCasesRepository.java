e0ebf0de51a323c356537be47388654c8fcdfce0
User <pcyuen98@gmail.com>
2021-03-15 18:41:38 +0800

    Updates

--- covid-web/src/main/java/com/app/repository/covid/CovidCasesRepository.java
+++ covid-web/src/main/java/com/app/repository/covid/CovidCasesRepository.java
@@ -3,6 +3,7 @@ package com.app.repository.covid;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -16,4 +17,11 @@ public interface CovidCasesRepository extends JpaRepository<CovidCasesAreaEntity
 	@Query(value = "SELECT DISTINCT c.date, c.cases, c.id, c.fk_area_id FROM trx_covid_cases AS c order by date desc LIMIT 5", nativeQuery = true)
 	List<CovidCasesAreaEntity> listLast5Records();
 	
+	// TODO: Practical bonus
+	// Change the Query above to Spring JPA Query or JPQL
+	// Simple SQL Should not use native query as change of db the query syntax need to be changed. 
+	// If use Spring JPQL, the SQL below can be maintained even change of DB
+	
+	@Query(value = "SELECT c from CovidCasesAreaEntity as c order by c.date desc")
+	List<CovidCasesAreaEntity> listLast5RecordsHQL(Pageable pageable);
 }
