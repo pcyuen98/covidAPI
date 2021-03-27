940843e40b815fffa93b63c2d76c213aa32c96f6
User <pcyuen98@gmail.com>
2021-03-27 18:33:18 +0800

    Update

--- covid-model/src/main/java/com/app/entity/CovidCasesBonusEntity.java
+++ covid-model/src/main/java/com/app/entity/CovidCasesBonusEntity.java
@@ -1,13 +1,26 @@
 package com.app.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
 
 //TODO: Practical bonus final
 
-//complete this as new CovidCasesBonusEntity
-
-@Table(name = "trx_covid_cases_bonus")
 //Hint Entity is similar to CovidCasesDescEntity
+//complete this as new CovidCasesBonusEntity
+@Entity
+@Table(name = "trx_covid_cases_bonus")
+@Getter
+@Setter
 public class CovidCasesBonusEntity {
+	
+	@Id
+	@GeneratedValue
+	private Long id;
 
+	private String description;
 }
--- covid-model/src/main/java/com/app/model/CovidCasesBonus.java
+++ covid-model/src/main/java/com/app/model/CovidCasesBonus.java
@@ -1,7 +1,13 @@
 package com.app.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 //TODO: Practical bonus final
 //complete this to usable POJO 
+
+@Getter
+@Setter
 public class CovidCasesBonus {
 
 	private Long id;
