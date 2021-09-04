38f005e320d228f2209f95658885b8edd6a07906
User <pcyuen98@gmail.com>
2021-09-04 21:37:28 +0800

    Updates

---
+++ covid-model/src/main/java/com/app/util/Mapper.java
@@ -0,0 +1,27 @@
+package com.app.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.app.entity.CovidCasesAreaEntity;
+import com.app.error.IDNotFoundException;
+import com.app.model.CovidCasesArea;
+
+public class Mapper {
+
+	// TODO: Practical Bonus advance 1 // implement the for loop below to return
+	// covidCasesAreas.
+	public static List<CovidCasesArea> asResources(List<CovidCasesAreaEntity> covidCaseEntities) {
+		List<CovidCasesArea> covidCasesAreas = new ArrayList<CovidCasesArea>();
+		if (covidCaseEntities == null) {
+			throw new IDNotFoundException(0L);
+		} else {
+
+			for (CovidCasesAreaEntity covidCasesEntity : covidCaseEntities) {
+			}
+		}
+
+		return covidCasesAreas;
+	}
+
+}
